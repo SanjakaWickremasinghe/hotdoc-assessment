b63353612cf767f369ba337f1cd6db32ed83c74b
SanjakaWickremasinghe <100257451+SanjakaWickremasinghe@users.noreply.github.com>
2022-03-09 23:49:47 +1100

    Update 2-even.js

--- js/tasks/2-even.js
+++ js/tasks/2-even.js
@@ -6,5 +6,12 @@
  * @returns {Boolean} Whether the input is even.
  */
 export default function isNumberEven(i) {
-
+    let even = false;
+    
+    if(i%2==0){
+        even = true;
+        return even;
+    } else {
+        return even;
+    }
 }
